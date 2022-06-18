48593205d08fd77f2e433cb24eb44822a4954f12
Yannic Holländer <yannic.hollaender@gmail.com>
2022-06-18 09:48:32 +0200

    Tutorial Bar Chart

--- src/localMessage.js
+++ src/localMessage.js
@@ -1,51 +1,358 @@
-/**
- * This file provides the mock "data" received
- * by your visualization code when you develop
- * locally.
- *
- */
 export const message = {
-  tables: {
-    DEFAULT: [
+  "tables": {
+    "DEFAULT": [
       {
-        dimID: ['25-54'],
-        metricID: [128863172],
+        "dimension": [
+          "MAZ"
+        ],
+        "metric": [
+          "878115"
+        ]
       },
       {
-        dimID: ['0-14'],
-        metricID: [61175933],
+        "dimension": [
+          "HAZ"
+        ],
+        "metric": [
+          "852484"
+        ]
       },
       {
-        dimID: ['65+'],
-        metricID: [51055052],
+        "dimension": [
+          "LVZ"
+        ],
+        "metric": [
+          "763851"
+        ]
       },
       {
-        dimID: ['15-24'],
-        metricID: [43351778],
+        "dimension": [
+          "KN"
+        ],
+        "metric": [
+          "551543"
+        ]
       },
       {
-        dimID: ['55-64'],
-        metricID: [42179856],
+        "dimension": [
+          "RND"
+        ],
+        "metric": [
+          "545845"
+        ]
       },
-    ],
+      {
+        "dimension": [
+          "OZ"
+        ],
+        "metric": [
+          "530830"
+        ]
+      },
+      {
+        "dimension": [
+          "LN"
+        ],
+        "metric": [
+          "456686"
+        ]
+      },
+      {
+        "dimension": [
+          "SB"
+        ],
+        "metric": [
+          "404094"
+        ]
+      },
+      {
+        "dimension": [
+          "RR"
+        ],
+        "metric": [
+          "348016"
+        ]
+      },
+      {
+        "dimension": [
+          "GT-ET"
+        ],
+        "metric": [
+          "275734"
+        ]
+      },
+      {
+        "dimension": [
+          "NP"
+        ],
+        "metric": [
+          "256604"
+        ]
+      },
+      {
+        "dimension": [
+          "AZ-WAZ"
+        ],
+        "metric": [
+          "221023"
+        ]
+      },
+      {
+        "dimension": [
+          "SN"
+        ],
+        "metric": [
+          "205292"
+        ]
+      },
+      {
+        "dimension": [
+          "DNN"
+        ],
+        "metric": [
+          "156873"
+        ]
+      },
+      {
+        "dimension": [
+          "PAZ"
+        ],
+        "metric": [
+          "127287"
+        ]
+      },
+      {
+        "dimension": [
+          "kuestenfischer"
+        ],
+        "metric": [
+          "73498"
+        ]
+      },
+      {
+        "dimension": [
+          "jobsfuerniedersachsen"
+        ],
+        "metric": [
+          "51278"
+        ]
+      },
+      {
+        "dimension": [
+          "lvz_job"
+        ],
+        "metric": [
+          "17274"
+        ]
+      }
+    ]
   },
-  fields: {
-    dimID: [
+  "dateRanges": {},
+  "fields": {
+    "dimension": [
       {
-        id: 'qt_nzqx6a0xvb',
-        name: 'Age Group',
-        type: 'TEXT',
-        concept: 'DIMENSION',
-      },
+        "id": "qt_qs1okoazvc",
+        "name": "website",
+        "type": "TEXT",
+        "concept": "DIMENSION"
+      }
     ],
-    metricID: [
+    "metric": [
       {
-        id: 'qt_8isx6a0xvb',
-        name: 'Population',
-        type: 'NUMBER',
-        concept: 'METRIC',
-      },
-    ],
+        "id": "qt_jk2okoazvc",
+        "name": "Record Count",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
   },
-  style: {},
+  "style": {
+    "barColor": {
+      "value": {
+        "color": "#1e555c"
+      },
+      "defaultValue": {
+        "color": "#1e555c"
+      }
+    }
+  },
+  "theme": {
+    "themeFillColor": {
+      "color": "#ffffff",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#e0e0e0",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#f15a60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7ac36a",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5a9bd4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#faa75a",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9e67ab",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#ce7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#d77fb3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81d4fa",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#f48fb1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#d1d1d1"
+    }
+  },
+  "interactions": {},
+  "colorMap": {}
 };
